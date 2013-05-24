109ed4e27a02dd10508d1144038f18673fe8a810
Oleg Kislitsyn <olegafx@live.com>
2013-05-24 14:33:43 +0800

    add new feeds, improve filter, fix line-height

--- js/app.js
+++ js/app.js
@@ -3,6 +3,6 @@
 angular.module('eNEWSey', ['ui.bootstrap', 'eNEWSey.controllers', 'eNEWSey.directives', 'eNEWSey.filters', 'eNEWSey.services', 'ngSanitize'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/', {templateUrl: 'partials/feedsList.html'})
+            .when('/', {templateUrl: 'partials/feedList.html'})
             .otherwise({redirectTo: '/'})
     }]);
