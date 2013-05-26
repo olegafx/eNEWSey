6154bcf1c2768ba73999fff7a53f120d89678341
Oleg Kislitsyn <olegafx@live.com>
2013-05-26 14:44:58 +0800

    chore() turn html5Mode on

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('eNEWSey', ['ui.bootstrap', 'eNEWSey.controllers', 'eNEWSey.directives', 'eNEWSey.filters', 'eNEWSey.services', 'ngSanitize'])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
         $routeProvider
             .when('/', {templateUrl: 'partials/feedList.html'})
             .otherwise({redirectTo: '/'})
