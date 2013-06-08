c47b181ac48c95dabd9528f91494803edb01a6ac
Oleg Kislitsyn <olegafx@live.com>
2013-06-08 11:19:57 +0800

    chore(app.js) remove ui-bootstrap.js dependency

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('eNEWSey', ['ui.bootstrap', 'eNEWSey.controllers', 'eNEWSey.directives', 'eNEWSey.filters', 'eNEWSey.services', 'ngSanitize'])
+angular.module('eNEWSey', ['eNEWSey.controllers', 'eNEWSey.directives', 'eNEWSey.filters', 'eNEWSey.services', 'ngSanitize'])
     .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider
