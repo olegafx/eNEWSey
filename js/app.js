5c0317b3f93e06c700e1ffba328b9c9859b61e0b
Oleg Kislitsyn <olegafx@live.com>
2013-06-08 11:05:49 +0800

    chore(main.min.js) replace js files with minified version

--- js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('eNEWSey', ['ui.bootstrap', 'eNEWSey.controllers', 'eNEWSey.directives', 'eNEWSey.filters', 'eNEWSey.services', 'ngSanitize'])
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', {templateUrl: 'partials/feedList.html'})
-            .otherwise({redirectTo: '/'})
-    }]);
