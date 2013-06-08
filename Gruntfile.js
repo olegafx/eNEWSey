49371b6f0e3809ce71450e4737f281de7236d7c8
Oleg Kislitsyn <olegafx@live.com>
2013-06-08 10:15:02 +0800

    chore(Grunt) add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        jshint : {
+            files : ['Gruntfile.js', 'js/main.js'],
+            options : {
+                validthis : true,
+                laxcomma : true,
+                laxbreak : true,
+                browser : true,
+                eqnull : true,
+                debug : true,
+                devel : true,
+                boss : true,
+                expr : true,
+                asi : true,
+                globals : {
+                    jQuery : true
+                }
+            }
+        },
+        qunit : {
+            all : {
+                options : {
+                    urls : ['http://localhost/tests/index.html']
+                }
+            }
+        },
+        concat : {
+            dist : {
+                src : ['js/jquery-1.9.1.min.js', 'js/main.js'],
+                dest : 'js/concat.js'
+            }
+        },
+        uglify : {
+            options : {
+                mangle : {
+                    except : ['jQuery']
+                }
+            },
+            my_target : {
+                files : {
+                    'js/main.min.js' : ['js/concat.js']
+                }
+            }
+        },
+        mincss : {
+            compress : {
+                files : {
+                    "css/main.min.css" : ['css/main.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-mincss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'mincss']);
+
+};
