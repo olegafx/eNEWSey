b07ec4e55e3cbe3d74a6cffbcd944c83930fd548
Oleg Kislitsyn <olegafx@live.com>
2013-06-09 08:55:12 +0800

    chore(Gruntfile.js) add nodemon and concurrent tasks, add grunt node alias for concurrent task

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,21 @@ module.exports = function (grunt) {
                     livereload: true
                 }
             }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'scripts/web-server.js'
+                }
+            }
+        },
+        concurrent: {
+            target: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
         }
     });
 
@@ -70,7 +85,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
 
+    grunt.registerTask('node', ['concurrent']);
     grunt.registerTask('test', ['jshint', 'qunit']);
     grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
